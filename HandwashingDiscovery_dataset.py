2cee66cc712211a39e3a1161e806760154d57f07
Ankita-30 <35312349+Ankita-30@users.noreply.github.com>
2022-11-16 15:04:36 -0800

    Update HandwashingDiscovery_dataset.py

--- HandwashingDiscovery_dataset.py
+++ HandwashingDiscovery_dataset.py
@@ -5,7 +5,7 @@
 import pandas as pd
 
 # Read datasets/yearly_deaths_by_clinic.csv into yearly
-yearly = pd.read_csv("datasets/yearly_deaths_by_clinic.csv")
+yearly = pd.read_csv("Datasets/yearly_deaths_by_clinic.csv")
 
 # Print out yearly
 print(yearly)
